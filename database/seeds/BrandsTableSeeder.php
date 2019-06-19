f43b0163e07e5f012b1851ebadef699f65cb7609
loulouz <dries3003@gmail.com>
2019-06-19 22:05:06 +0200

    versie 19-6 packes unable to install

---
+++ database/seeds/BrandsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BrandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        factory('App\Brand',10)->create();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
